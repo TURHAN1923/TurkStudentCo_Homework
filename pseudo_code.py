c01bf35f39519c8f13d61595ba55572569924530
TURHAN1923 <emreturhanyzm@gmail.com>
2024-09-15 13:52:09 +0300

    Create pseudo_code.py

---
+++ pseudo_code.py
@@ -0,0 +1,56 @@
+"""
+1. soru;
+Başla
+İlk sayiyi kullanicidan al ve 'x' degiskenine ata
+ikinci sayiyi kullanicidan al ve 'y' degiskenine ata
+'x' ve 'y' degiskenlerini topla ve 'total' degiskenine ata
+'total' degiskenini ekrana yazdır.
+Bitir.
+"""
+
+
+"""
+2.soru;
+Başla
+1)'total' degiskenini 0 olarak baslat
+2)'i' degiskenini 1 olarak baslat
+3)eger 'i' 100 e esit ya da kucukse
+    'total' = 'total'+'i'
+    'i' yi 1 artir.
+     3. adima geri don
+4)'total' degerini ekrana yazdir
+Bitir.
+"""
+
+
+"""
+3. soru;
+Basla
+Başla
+    1. Kullanıcıdan bir sayı al ve 'a' değişkenine ata.
+    2. Eğer (a % 2 == 0) ise
+        a = "çift sayıdır" yazdır.
+    3. Değilse
+        a = "tek sayıdır" yazdır.
+Bitir
+"""
+
+
+"""
+4.soru;
+Başla
+    1. 'dizi' adında bir dizi oluştur.
+    2. Boş bir 'kontrol' kümesi oluştur.
+    3. Her bir 'eleman' için 'dizi' içinde döngü başlat:
+        3.1 Eğer 'eleman' 'kontrol' kümesinde varsa:
+            "Tekrarlayan eleman bulundu" yazdır.
+            Programı bitir.
+        3.2 Eğer 'eleman' 'kontrol' kümesinde yoksa:
+            'eleman'ı 'kontrol' kümesine ekle.
+    4. Eğer döngü bittiyse:
+        "Tekrarlayan eleman yok" yazdır.
+Bitir
+
+
+
+"""
